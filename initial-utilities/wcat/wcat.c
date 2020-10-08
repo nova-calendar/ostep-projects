ea89014a65fa171cf91f3469c38bd011afe59ead
nova <nova-calendar@protonmail.com>
2020-10-08 14:34:17 -0600

    turns out having misc printf and other testing code breaks the test script. all works as intended and passes all 7 tests

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -13,17 +13,6 @@ int main(int argc, char *argv[])
 
 	FILE *fp;
 
-	printf("\n====\twcat\t====\n");
-
-	printf("\nnumber of files to read: %d\n", argc-1);
-
-	printf("files to be read: ");
-	for (i=1; i<argc; i++) {
-		printf("%s\t", argv[i]);
-	}
-
-	printf("\n");
-
 	for (i=1; i<argc; i++) {
 		fp = fopen(argv[i], "r");
 
@@ -32,7 +21,6 @@ int main(int argc, char *argv[])
 			exit(1);
 		}
 
-		printf("\nOpening %s:\n", argv[i]);
 
 		while ( !feof(fp) ) {
 			if (fgets(buff, 80, fp) != NULL) {
