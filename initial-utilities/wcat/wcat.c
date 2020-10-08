524f74d18a0217ef60d8181fe87273161d1bae8d
nova <nova-calendar@protonmail.com>
2020-10-08 14:28:50 -0600

    testing failed but wcat seemingly works

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAXLENGTH 1000
 
 
 int main(int argc, char *argv[]) 
 {
 
 	int i;
+
+	char buff[80];
+
+	FILE *fp;
 
 	printf("\n====\twcat\t====\n");
 
@@ -15,6 +22,26 @@ int main(int argc, char *argv[])
 		printf("%s\t", argv[i]);
 	}
 
-	return 0;
+	printf("\n");
 
+	for (i=1; i<argc; i++) {
+		fp = fopen(argv[i], "r");
+
+		if (fp == NULL) {
+			printf("wcat: cannot open file\n");
+			exit(1);
+		}
+
+		printf("\nOpening %s:\n", argv[i]);
+
+		while ( !feof(fp) ) {
+			if (fgets(buff, 80, fp) != NULL) {
+				printf("%s", buff);
+			}
+		}
+		fclose(fp);
+	}
+
+
+	return 0;
 }
